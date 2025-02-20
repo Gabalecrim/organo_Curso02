c801f5da44d6740f3c2c1a738617c72412ae5a4e
Gabriel Alecrim <eu@gabrielalecrim.com.br>
2025-02-20 16:12:41 -0300

    Aula 02 - Componentes

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -1,15 +1,20 @@
-import './colaborador.css'
+import { IoClose } from "react-icons/io5";
 
-const Colaborador = ({ colaborador, corDeFundo }) => {
-    return (<div className="colaborador">
-        <div className="cabecalho" style={{ backgroundColor: corDeFundo }}>
-            <img src={colaborador.imagem} alt={colaborador.nome} />
-        </div>
-        <div className="rodape">
-            <h4>{colaborador.nome}</h4>
-            <h5>{colaborador.cargo}</h5>
-        </div>
-    </div>)
-}
+import "./colaborador.css";
 
-export default Colaborador
+const Colaborador = ({ colaborador, corDeFundo, aoDeletar }) => {
+  return (
+    <div className="colaborador">
+      <IoClose size={25} color="#fff" className="deletar" onClick={aoDeletar} />
+      <div className="cabecalho" style={{ backgroundColor: corDeFundo }}>
+        <img src={colaborador.imagem} alt={colaborador.nome} />
+      </div>
+      <div className="rodape">
+        <h4>{colaborador.nome}</h4>
+        <h5>{colaborador.cargo}</h5>
+      </div>
+    </div>
+  );
+};
+
+export default Colaborador;
